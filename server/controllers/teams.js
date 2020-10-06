40c475be0bade07c87a4694f0efc91983ae879e8
FbFDestro <fbfdestro@gmail.com>
2020-10-06 18:19:28 -0300

    feat: getTeams implemented
    Controller that responds to a get request for teams.
    
    Responds the following data from every team:
    
    team_id,
    name,
    logo_url,
    wins,
    losses,
    points,
    opponent_stat_points,
    winning_percentage,
    points_per_game,
    opponent_points_per_game,
    points_per_game_difference

--- server/controllers/teams.js
+++ server/controllers/teams.js
@@ -1,9 +1,34 @@
 const query = require('../databaseQuery');
 
 const getTeams = async (request, response) => {
-  const data = await query('select * from teams limit 2');
-  console.log(data);
-  return response.status(200).json('god');
+  const attributes = [
+    't.team_id',
+    'ts.name',
+    't.wikipedia_logo_url as logo_url',
+    'ts.wins',
+    'ts.losses',
+    'ts.games',
+    'ts.points',
+    'ts.opponent_stat_points',
+  ];
+  const from = `teams t inner join team_stats ts on t.team_id = ts.team_id`;
+  const queryResponse = await query(attributes, from, '');
+
+  if (queryResponse.error === null) {
+    // no error from database
+    const { data } = queryResponse;
+    for (team of data) {
+      team['winning_percentage'] = team['wins'] / team['games'];
+      team['points_per_game'] = team['points'] / team['games'];
+      team['opponent_points_per_game'] = team['opponent_stat_points'] / team['games'];
+      team['points_per_game_difference'] =
+        team['points_per_game'] - team['opponent_points_per_game'];
+
+      delete team['games']; // only used to calculate other values
+    }
+  }
+
+  return response.status(200).json(queryResponse);
 };
 
 module.exports = {
